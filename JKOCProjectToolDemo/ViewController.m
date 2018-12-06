8063c22e6ee64e652f237f867b0f9ec0d8485dd7
JoanKingWC <JoanKingWC@163.com>
2018-12-06 18:29:29 +0800

    更新内容

--- JKOCProjectToolDemo/ViewController.m
+++ JKOCProjectToolDemo/ViewController.m
@@ -18,11 +18,9 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(50, 200,JK_SCREEN_WIDTH-100 , 100)];
     view.layer.cornerRadius = 50;
     [view jk_setGradientBackgroundWithColors:@[JKRGBCOLOR(255,219,0,1),JKRGBCOLOR(255,185,17,1)] locations:nil startPoint:CGPointMake(0, 0) endPoint:CGPointMake(1, 0)];
-    
     [view jk_addShadowToViewShadowRadius:4 withColor:JKRGBCOLOR(255,185,17,1) withShadowOffset:CGSizeMake(0, 2) withShadowOpacity:0.48];
     
     [self.view addSubview:view];
